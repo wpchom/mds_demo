75c156defefb214961a5618a605c072b62a44bf8
pchom <pchom@hotmail.com>
2024-05-30 16:59:23 +0000

    update stm32f103 demo

--- project/stm32f103/src/application/app_main.c
+++ project/stm32f103/src/application/app_main.c
@@ -11,15 +11,21 @@ void InitThread(MDS_Arg_t *arg)
     }
 }
 
+void BOARD_CLOCK_Init(void)
+{
+}
+
 int main(void)
 {
-    MDS_KernelInit();
-
-    SEGGER_SYSVIEW_Conf();
+    BOARD_CLOCK_Init();
 
     MDS_CoreInterruptRequestRegister(SysTick_IRQn, (MDS_IsrHandler_t)MDS_SysTickIncCount, NULL);
     MDS_CoreInterruptRequestEnable(SysTick_IRQn);
     SysTick_Config(SystemCoreClock / MDS_SYSTICK_FREQ_HZ);
+
+    SEGGER_SYSVIEW_Conf();
+
+    MDS_KernelInit();
 
     MDS_Thread_t *thread = MDS_ThreadCreate("init", InitThread, NULL, 1024, 10, 10);
     if (thread != NULL) {
